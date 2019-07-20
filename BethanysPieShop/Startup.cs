ac8e144475d1f05a7c91c4d4e36bd0fcc3b8ccb3
jtavenner <username@users.noreply.github.com>
2019-07-20 17:52:58 -0400

    Complete chapter 6.  Began adding navigation through routing.  Added Details view.

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -38,7 +38,13 @@ namespace BethanysPieShop
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
-            app.UseMvcWithDefaultRoute();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                    );
+            });
         }
     }
 }
